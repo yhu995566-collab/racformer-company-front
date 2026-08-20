eb914234a512f5032caa1d21d605fc16f3c7ab62
yanhao <yhu995566@gmail.com>
2026-08-20 16:00:15 +0800

    fix: keep environment helper private in MMCV config

--- configs/3dh_query_company_20260818_f4.py
+++ configs/3dh_query_company_20260818_f4.py
@@ -1,10 +1,10 @@
-import os
+import os as _os
 
 _base_ = ['./3dh_query_company_front_350m_f4.py']
 
 # Synchronized ChengTech capture from 2026-08-18. Images are undistorted
 # offline at their native 1920x1080 resolution before this augmentation.
-dataset_root = os.path.abspath(os.environ.get(
+dataset_root = _os.path.abspath(_os.environ.get(
     'RACFORMER_COMPANY_PROCESSED_ROOT',
     '/mnt/diskNvme1/DataSet/radar_camera_GT/'
     '2026-08-18-16-01-30/processed_racformer')) + '/'
